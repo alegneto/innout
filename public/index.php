8169f3fa1a09c4fc703a84a6029b869763a91bc9
Alexandre Gonçalves Neto <alegneto@hotmail.com>
2020-08-10 23:21:13 -0300

    Redirecionamento #02

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
-$uri = urldecode($_SERVER['REQUEST_URI']);
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if ($uri === '' or $uri === '/' or $uri === '/index.php') {
 	$uri = '/login.php';
