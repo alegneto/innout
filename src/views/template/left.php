7b182dec8069b8abc9b547ca50d5ba345014de48
Alexandre Gonçalves Neto <alegneto@hotmail.com>
2020-08-10 23:41:24 -0300

    Barra Lateral (Menu)

---
+++ src/views/template/left.php
@@ -0,0 +1,30 @@
+<aside class="sidebar">
+	<nav class="menu mt-3">
+		<ul class="nav-list">
+			<li class="nav-item">
+				<a href="day_records.php">
+					<i class="icofont-ui-check mr-2"></i>
+					Registrar Ponto
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href=".php">
+					<i class="icofont-ui-calendar mr-2"></i>
+					Relatorio Mensal
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href=".php">
+					<i class="icofont-chart-histogram mr-2"></i>
+					Relatorio Gerencial
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href=".php">
+					<i class="icofont-users mr-2"></i>
+					Usuários
+				</a>
+			</li>
+		</ul>
+	</nav>
+</aside>
