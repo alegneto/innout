d9327efb39ddf449dec078c74e26f9d3853fa490
Alexandre Gonçalves Neto <alegneto@hotmail.com>
2020-08-18 22:26:56 -0300

    Classe Working Hours

--- src/models/Model.php
+++ src/models/Model.php
@@ -63,6 +63,18 @@ class Model
 		}
 	}
 
+	public function save()
+	{
+		$sql = "INSERT INTO " . static::$tableName . " ("
+			. implode(",", static::$columns) . ") VALUES (";
+		foreach (static::$columns as $col) {
+			$sql .= static::getFormatedValue($this->$col) . ",";
+		}
+		$sql[strlen($sql) - 1] = ')';
+		$id = Database::executeSQL($sql);
+		$this->id = $id;
+	}
+
 	public static function getFilters($filters)
 	{
 		$sql = '';
