038e95c9ec566bc1a4fa510eeb342b783d219f06
Alexandre Gonçalves Neto <alegneto@hotmail.com>
2020-08-22 00:41:00 -0300

    Relatório Mensal #03

--- src/controllers/monthly_report.php
+++ src/controllers/monthly_report.php
@@ -2,9 +2,39 @@
 session_start();
 requireValidSession();
 
+$currentDate = new DateTime();
+
 $user = $_SESSION['user'];
 $registries = WorkingHours::getMonthlyReport($user->id, new DateTime());
 
+$report = [];
+$workDay = 0;
+$sumOfWorkedTime = 0;
+$lastDay = getLastDayOfMonth($currentDate)->format('d');
+
+for ($day = 1; $day <= $lastDay; $day++) {
+	$date = $currentDate->format('Y-m') . '-' . sprintf('%02d', $day);
+	$registry = $registries[$date];
+
+	if (isPastWorkday($date)) $workDay++;
+
+	if ($registry) {
+		$sumOfWorkedTime += $registry->worked_time;
+		array_push($report, $registry);
+	} else {
+		array_push($report, new WorkingHours([
+			'work_date' => $date,
+			'worked_time' => 0
+		]));
+	}
+}
+
+$expectedTime = $workDay * DAYLY_TIME;
+$balance = getTimeSrtingFromSeconds(abs($sumOfWorkedTime - $expectedTime));
+$sign = ($sumOfWorkedTime >= $expectedTime) ? '+' : '-';
+
 loadTemplateView('monthly_report', [
-	'registries' => $registries
+	'report' => $report,
+	'sumOfWorkedTime' => $sumOfWorkedTime,
+	'balance' => "{$sign}{$balance}"
 ]);
